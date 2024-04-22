0891b3c71c06fa917e1a5c16905bf1ebf062e225
oldmcdonnell <oldmcdonnell@gmail.com>
2024-04-22 14:41:04 -0400

    button working

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom"
+import { useState } from "react";
 
 const Title = () => {
   return (
@@ -8,14 +9,32 @@ const Title = () => {
   )
 }
 
+
+const CounterButton = () => {
+  
+  const [count, setCount] = useState(0);
+  console.log(count)
+  const handleClick = () => {
+    const newCount = count+1
+    setCount(newCount) 
+  }
+  return(
+    <button onClick={handleClick} className="dark:color-white rounded-lg bg=purple-700 px-2 py-1 fous:ring active:bg-purple-600">click count: {count}</button>
+  )
+}
+
+
 function App() {
   return (
     <div 
       className="bg-primary h-100 p-5"
       style={{ color: 'white' }}
-    >
+    > 
+    <CounterButton />
+    <div></div>
       <Link className="text-light" to='/about'>About</Link>
       <Title />
+
     </div>
   )
 }
--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig(({}) => {
   return {
-    base: '/your-repo-name/',
+    base: '/react-intro/',
     plugins: [react()],
     server: {
       port: 8080,
